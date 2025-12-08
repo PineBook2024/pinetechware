24f6be30e74e36c4ae2c978ad660be88835708f7
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2025-12-09 00:37:15 +0500

    new versal build update

--- app/utils/LenisProvider.js
+++ app/utils/LenisProvider.js
@@ -1,27 +1,24 @@
 "use client";
 
 import { ReactNode, useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function LenisProvider({ children }) {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
+      easing: (t) => t,
       smooth: true,
-      lerp: 0.1,
     });
 
     function raf(time) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
-
     requestAnimationFrame(raf);
 
-    return () => {
-      lenis.destroy(); // cleanup to avoid conflicts
-    };
+    return () => lenis.destroy();
   }, []);
 
-  return <>{children}</>;
+  return children;
 }
