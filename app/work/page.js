06f356043b27f5895941413e1d62138a7f4e6b43
unknown <zainabbasashtar55@gmail.com>
2026-02-10 04:13:42 +0500

    scroll slider

--- app/work/page.js
+++ app/work/page.js
@@ -1,4 +1,5 @@
 'use client'
+
 import { useState } from "react";
 import Image from 'next/image'
 import Featured from '@/components/Featured/Featured'
@@ -8,80 +9,72 @@ import Sidepopup from '@/components/Sidepopup/Sidepopup'
 import { usePopup } from "@/context/PopupContext";
 import SeoHead from "@/components/SeoHead";
 import Trigger from '@/components/Trigger/Trigger'
-
-
 import React from 'react'
 
 export default function work() {
     const { openPopup, closePopup, isOpen } = usePopup();
+    const [fullscreenImage, setFullscreenImage] = useState(null);
 
-   const tabData = {
-  "Web Development": {
-    images: [
-      "/images/Portfolio/Web-Development/pack-1-1.webp",
-      "/images/Portfolio/Web-Development/pack-2-2.webp",
-      "/images/Portfolio/Web-Development/pack-3-3.webp",
-      "/images/Portfolio/Web-Development/pack-4-4.webp",
-      "/images/Portfolio/Web-Development/pack-5-5.webp",
-      "/images/Portfolio/Web-Development/pack-6-6.webp",
-      "/images/Portfolio/Web-Development/pack-7-7.webp",
-      "/images/Portfolio/Web-Development/pack-8-8.webp",
-      "/images/Portfolio/Web-Development/pack-9-9.webp",
-    ],
-  },
-
-  "Branding": {
-    images: [
-      "/images/Portfolio/Branding/branding-1.webp",
-      "/images/Portfolio/Branding/branding-2.webp",
-      "/images/Portfolio/Branding/branding-3.webp",
-      "/images/Portfolio/Branding/branding-4.webp",
-      "/images/Portfolio/Branding/branding-5.webp",
-      "/images/Portfolio/Branding/branding-6.webp",
-      "/images/Portfolio/Branding/branding-7.webp",
-      "/images/Portfolio/Branding/branding-8.webp",
-      "/images/Portfolio/Branding/branding-9.webp",
-    ],
-  },
-
-  "Logo": {
-    images: [
-      "/images/Portfolio/Logo/logo-1.webp",
-      "/images/Portfolio/Logo/logo-2.webp",
-      "/images/Portfolio/Logo/logo-3.webp",
-      "/images/Portfolio/Logo/logo-4.webp",
-      "/images/Portfolio/Logo/logo-5.webp",
-      "/images/Portfolio/Logo/logo-6.webp",
-      "/images/Portfolio/Logo/logo-7.webp",
-      "/images/Portfolio/Logo/logo-8.webp",
-      "/images/Portfolio/Logo/logo-9.webp",
-    ],
-  },
-
-  "App Development": {
-    images: [
-      "/images/Portfolio/App-Development/app-1.webp",
-      "/images/Portfolio/App-Development/app-2.webp",
-      "/images/Portfolio/App-Development/app-3.webp",
-      "/images/Portfolio/App-Development/app-4.webp",
-      "/images/Portfolio/App-Development/app-5.webp",
-      "/images/Portfolio/App-Development/app-6.webp",
-      "/images/Portfolio/App-Development/app-7.webp",
-      "/images/Portfolio/App-Development/app-8.webp",
-      "/images/Portfolio/App-Development/app-9.webp",
-    ],
-  },
-};
-
-
-
+    const tabData = {
+        "Web Development": {
+            images: [
+                "/images/Portfolio/Web-Development/pack-1-1.webp",
+                "/images/Portfolio/Web-Development/pack-2-2.webp",
+                "/images/Portfolio/Web-Development/pack-3-3.webp",
+                "/images/Portfolio/Web-Development/pack-4-4.webp",
+                "/images/Portfolio/Web-Development/pack-5-5.webp",
+                "/images/Portfolio/Web-Development/pack-6-6.webp",
+                "/images/Portfolio/Web-Development/pack-7-7.webp",
+                "/images/Portfolio/Web-Development/pack-8-8.webp",
+                "/images/Portfolio/Web-Development/pack-9-9.webp",
+            ],
+        },
+        "Branding": {
+            images: [
+                "/images/Portfolio/Branding/branding-1.webp",
+                "/images/Portfolio/Branding/branding-2.webp",
+                "/images/Portfolio/Branding/branding-3.webp",
+                "/images/Portfolio/Branding/branding-4.webp",
+                "/images/Portfolio/Branding/branding-5.webp",
+                "/images/Portfolio/Branding/branding-6.webp",
+                "/images/Portfolio/Branding/branding-7.webp",
+                "/images/Portfolio/Branding/branding-8.webp",
+                "/images/Portfolio/Branding/branding-9.webp",
+            ],
+        },
+        "Logo": {
+            images: [
+                "/images/Portfolio/Logo/logo-1.webp",
+                "/images/Portfolio/Logo/logo-2.webp",
+                "/images/Portfolio/Logo/logo-3.webp",
+                "/images/Portfolio/Logo/logo-4.webp",
+                "/images/Portfolio/Logo/logo-5.webp",
+                "/images/Portfolio/Logo/logo-6.webp",
+                "/images/Portfolio/Logo/logo-7.webp",
+                "/images/Portfolio/Logo/logo-8.webp",
+                "/images/Portfolio/Logo/logo-9.webp",
+            ],
+        },
+        "App Development": {
+            images: [
+                "/images/Portfolio/App-Development/app-1.webp",
+                "/images/Portfolio/App-Development/app-2.webp",
+                "/images/Portfolio/App-Development/app-3.webp",
+                "/images/Portfolio/App-Development/app-4.webp",
+                "/images/Portfolio/App-Development/app-5.webp",
+                "/images/Portfolio/App-Development/app-6.webp",
+                "/images/Portfolio/App-Development/app-7.webp",
+                "/images/Portfolio/App-Development/app-8.webp",
+                "/images/Portfolio/App-Development/app-9.webp",
+            ],
+        },
+    };
 
     const tabKeys = Object.keys(tabData);
     const [activeTab, setActiveTab] = useState(tabKeys[0]);
 
     return (
         <div>
-
             <SeoHead
                 title="Our Work & Case Studies in Innovation | Pine Techware"
                 description="Explore Pine Techware's portfolio - Our real-world solutions across FinTech, E-commerce, and more. See how we turn ideas into best digital experiences."
@@ -95,7 +88,9 @@ export default function work() {
                 <div className="container xl:max-w-7xl mx-auto w-full px-6 pt-20 lg:pt-32 pb-16 lg:pb-32 lg:px-8">
                     <div className="max-w-3xl xl:max-w-7xl">
                         <div className="lg:overflow-hidden">
-                            <h2 className="font-bold sentence-first-letter text-4xl xl:text-5xl 2xl:text-6xl text-black lg:-translate-y-[7px] xl:!leading-[55px] 2xl:!leading-[65px]"><span> Happy Clients<br /> of Pine Techware </span></h2>
+                            <h2 className="font-bold sentence-first-letter text-4xl xl:text-5xl 2xl:text-6xl text-black lg:-translate-y-[7px] xl:!leading-[55px] 2xl:!leading-[65px]">
+                                <span> Happy Clients<br /> of Pine Techware </span>
+                            </h2>
                         </div>
 
                         <div className="w-full mx-auto mt-10">
@@ -106,8 +101,8 @@ export default function work() {
                                     <button
                                         key={tab}
                                         onClick={() => setActiveTab(tab)}
-                                        className={`px-4 py-2 font-semibold rounded-full cursor-pointer 
-                          ${activeTab === tab ? "bg-black text-white" : "bg-white border text-black"}`}
+                                        className={`px-4 py-2 font-semibold rounded-full cursor-pointer
+                                            ${activeTab === tab ? "bg-black text-white" : "bg-white border text-black"}`}
                                     >
                                         {tab}
                                     </button>
@@ -120,7 +115,8 @@ export default function work() {
                                     {tabData[activeTab].images.map((src, index) => (
                                         <div
                                             key={index}
-                                            className="relative w-full max-w-xs mx-auto aspect-[3/4]"
+                                            className="relative w-full max-w-xs mx-auto aspect-[3/4] group cursor-pointer"
+                                            onClick={() => setFullscreenImage(src)}
                                         >
                                             {/* Back card 1 */}
                                             <div className="absolute inset-0 rounded-3xl bg-white shadow-md opacity-40 transform translate-x-4 translate-y-4" />
@@ -134,50 +130,35 @@ export default function work() {
                                                         alt={`Web project ${index + 1}`}
                                                         width={400}
                                                         height={800}
-                                                        className="
-    w-full 
-    h-full 
-    object-cover 
-    object-top 
-    transition-all 
-    duration-[3500ms] 
-    ease-linear 
-  "
+                                                        className="w-full h-full object-cover object-top transition-transform duration-[3500ms] ease-linear
+                                                        group-hover:translate-y-[-50px]"
                                                     />
                                                 </div>
                                             </div>
                                         </div>
                                     ))}
                                 </div>
-
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
 
-            {/* <Featured /> */}
-
-            {/* <section className="start-project bg-dark relative w-full lg:min-h-[80vh] z-50 flex items-center justify-center top-[0px]">
-                <div className="mx-auto reveal-text text-center max-w-7xl md:pb-24 pb-14 lg:px-8">
-                    <p className="text-black mb-2 text-base md:text-md lg:text-lg xl:text-xl 2xl:text-2xl">Pull the Trigger!</p>
-                    <div className="overflow-hidden">
-                        <h2 className="text-black font-extrabold text-[43px] leading-none lg:text-6xl xl:text-7xl 2xl:text-8xl pb-2 2xl:pb-[11px]" style={{ translate: 'none', rotate: 'none', scale: 'none', transform: 'translate(0px, 0px)' }}>Ready to turn your ideas  <br className="min-[580px]:block hidden" />into impactful digital experiences?</h2>
-
-                         
-
-                    </div>
-                    <div className="flex mx-auto justify-center mt-6 md:mt-8 lg:mt-11 w-fit">
-                        <button
-                            className="buttonChange buttonChangeBlack bg-black text-white w-[180px] h-[45px] border-transparent rounded-full font-extrabold text-sm" onClick={openPopup}
-                        >
-                            <span className="pb-[3px] md:pb-1.5">Start Your Project</span>
-                            <span className="pb-[3px] md:pb-1.5">Start Your Project</span>
-                        </button>
-                    </div>
+            {/* Fullscreen Image Overlay */}
+            {fullscreenImage && (
+                <div
+                    className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50 cursor-pointer"
+                    onClick={() => setFullscreenImage(null)}
+                >
+                    <Image
+                        src={fullscreenImage}
+                        alt="Fullscreen"
+                        width={1200}
+                        height={1200}
+                        className="max-h-[90vh] max-w-[90vw] object-contain"
+                    />
                 </div>
-            </section> */}
-
+            )}
 
             <Trigger
                 title="Ready to turn your ideas into impactful digital experiences?"
@@ -185,10 +166,8 @@ export default function work() {
                 buttonText="Start Your Project"
             />
 
-
             <Sidepopup />
             <Footer />
-
         </div>
     )
 }
