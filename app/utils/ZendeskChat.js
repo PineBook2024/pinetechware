61685f05566786a749c67a77613f6bb4b2ee2e4a
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2026-05-13 16:29:20 -0700

    zendesk addsssssss

--- app/utils/ZendeskChat.js
+++ app/utils/ZendeskChat.js
@@ -19,10 +19,6 @@ export default function ZendeskChat() {
   }, []);
 
   return (
-    <Script
-      id="ze-snippet"
-      src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"
-      strategy="afterInteractive"
-    />
+    <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </script>
   );
 }
