ec1c76eb77e80d0d1fdbc54aac590773ab87ddd3
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2026-07-01 17:07:23 -0700

    revert changessfsddd

--- app/utils/ZendeskChat.js
+++ app/utils/ZendeskChat.js
@@ -1,49 +1,11 @@
-"use client";
-
-import { useEffect } from "react";
 import Script from "next/script";
 
-function showZendeskWidget() {
-  if (typeof window === "undefined") return false;
-
-  const isDesktop = window.innerWidth >= 768;
-
-  if (typeof window.zE === "function") {
-    try {
-      window.zE("webWidget", "show");
-      if (isDesktop) window.zE("webWidget", "open");
-      return true;
-    } catch {
-      return false;
-    }
-  }
-
-  if (window.$zopim?.livechat) {
-    window.$zopim.livechat.button.show();
-    if (isDesktop) window.$zopim.livechat.window.show();
-    return true;
-  }
-
-  return false;
-}
-
 export default function ZendeskChat() {
-  useEffect(() => {
-    const checkZendesk = setInterval(() => {
-      if (showZendeskWidget()) {
-        clearInterval(checkZendesk);
-      }
-    }, 1000);
-
-    return () => clearInterval(checkZendesk);
-  }, []);
-
   return (
     <Script
       id="ze-snippet"
       src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"
       strategy="afterInteractive"
-      onLoad={showZendeskWidget}
     />
   );
 }
